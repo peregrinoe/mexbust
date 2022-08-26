31f4ed58e04ea86fbe38c1d6756d260301f5aa1a
peregrinoe <s_nick_94@hotmail.com>
2022-08-26 15:29:38 -0500

    php

---
+++ terminales.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 ,user-scalable=no"/>
+    <title>Mexbust</title>
+    <link rel="shortcut icon" href="https://w7.pngwing.com/pngs/951/425/png-transparent-public-transport-switzerland-one-dance-bus-app-store-switzerland-text-logo-grass.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="mediaQueries.css">
+</head>
+<body>
+    <div class="container">
+        <div data-pokemon-info class="info-pokemon">
+            <div class="navbar">
+                <div class="navbar-list">
+                    <div class="div">Usuarios</div>
+                    <div class="div">Viaje de autobus</div>
+                    <div class="div">Terminales</div>
+                    <div class="div">Venta de boletos</div>
+                    <div class="div">Consultas</div>
+                </div>
+            </div>
+            <div data-pokemon-card class="home">
+                <div class="profile">
+                    <div class="photo"><img data-poke-image class="profile-photo" src="https://cdn.icon-icons.com/icons2/1508/PNG/512/systemusers_104569.png" alt="User Next"></div>
+                    <div class="info-profile">
+                        <div class="name">Nombre</div>
+                        <div class="surname">Noombree</div>
+                        <div class="phone">+52 55 5555 5555*</div>
+                    </div>
+                </div>
+            </div>  
+            <div data-pokemon-card class="home">
+                <div class="maps">
+                    <div class="routes">
+                        <div class="after-route">ROUTE 1 </div>
+                        <div class="before-route">RPUTE 2 </div>
+                    </div>
+                    <div class="location"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.5247266138467!2d-99.17598874934208!3d19.389721386844442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff76acf12761%3A0xa17520487cdc8e97!2zTsOhcG9sZXM!5e0!3m2!1ses!2smx!4v1661497801367!5m2!1ses!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                </div>
+            </div> 
+        </div>
+    </div>
+</body>
+</html>
